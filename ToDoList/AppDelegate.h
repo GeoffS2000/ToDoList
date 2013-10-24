52aa89e3abf1aa68bed5445b8dc2ae44d5b3b3bf
Geoffrey Spaeth <gspaeth@spaeth.local>
2013-10-24 16:33:48 -0700

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Geoffrey Spaeth on 10/24/13.
+//  Copyright (c) 2013 Geoffrey Spaeth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Geoffrey Spaeth on 10/24/13.
+//  Copyright (c) 2013 Geoffrey Spaeth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
